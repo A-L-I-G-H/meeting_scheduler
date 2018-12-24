e32dd50ed51f48ebb8478141ac8c6fddebeaf6ef
ahmad pourihosseini <seyedahmadpourihosseini@yahoo.com>
2018-12-25 00:05:32 +0330

    add a dummy API

---
+++ front_end/src/api/index.js
@@ -0,0 +1,12 @@
+const EventPolls = [
+    {title: "weeklyCommittee", description: "weekly session for ...", owner: "ghasem", is_finalized: false},
+    {title: "itST delivery", description: "syntax based testing", owner: "borna", is_finalized: true},
+    {title: "ACM monthly meeting", description: "for reviewing the schedule of the up-coming ...", owner: "ghazal", is_finalized: false},
+    {title: "AP delivery", description: "final assignment for AP", owner: "ahmad", is_finalized: false},
+    {title: "DM delivery", description: "implementation of dijkstra", owner: "ahmad", is_finalized: true},
+];
+
+const loggedInUser = {username: "ahmad"};
+
+
+export {EventPolls};
