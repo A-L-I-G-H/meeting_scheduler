ba2b0e8b9ce9d0d4dd66e50607be71ee13d69cde
ahmad pourihosseini <seyedahmadpourihosseini@yahoo.com>
2018-12-28 11:21:33 +0330

    add more colors to ColorTheme

--- front_end/src/globals.js
+++ front_end/src/globals.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 const ColorTheme = {
+    darkPrimaryColor: '#1976D2',
+    lightPrimaryColor: '#BBDEFB',
     primaryColor: '#2196F3',
-    secondaryColor: "#FF9800",
+    accentColor: "#FF9800",
+    secondaryText: '#757575',
+    divierColor: '#BDBDBD',
+    passiveElement: '#757575',
 };
 
 const HistoryContext = React.createContext();
