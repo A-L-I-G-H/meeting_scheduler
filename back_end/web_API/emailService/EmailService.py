74c82f83bd4b589d2a424af0393b3d79eced3d4b
A-L-I-G-H <34342988+A-L-I-G-H@users.noreply.github.com>
2018-12-29 02:20:13 +0330

    asynchronous e-mail service working

--- back_end/web_API/emailService/EmailService.py
+++ back_end/web_API/emailService/EmailService.py
@@ -1,6 +1,18 @@
+import threading
 from django.core.mail import send_mail
 
 sender_mail = "meetingschedulerforyou@gmail.com"
 
-def send_email_to_users(subject, body, email_list):
-    send_mail(subject, body, sender_mail, email_list)
+class EmailServiceThread(threading.Thread):
+    def __init__(self, subject, content, recipient_list):
+        self.subject = subject
+        self.recipient_list = recipient_list
+        self.content = content
+        threading.Thread.__init__(self)
+
+    def run (self):
+        send_mail(self.subject, self.content, sender_mail, self.recipient_list)
+
+
+def send_email_to_users(subject, content, recipient_list):
+    EmailServiceThread(subject, content, recipient_list).start()
