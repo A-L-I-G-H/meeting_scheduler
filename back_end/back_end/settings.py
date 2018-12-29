8aeb6a8e31324c741ca54e9b29662756470e8ac9
A-L-I-G-H <34342988+A-L-I-G-H@users.noreply.github.com>
2018-12-29 15:35:11 +0330

    email service is not testable

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -126,3 +126,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'meetingschedulerforyou@gmail.com'
 EMAIL_HOST_PASSWORD = 'meeting@123'
+
--- back_end/web_API/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from web_API.emailService.EmailService import *
-
-class EmailServiceTest(TestCase):
-
-    def test_email_sending(self):
-        send_email_to_users('test subject', 'test body', ['aligharizadeh@outlook.com'])
