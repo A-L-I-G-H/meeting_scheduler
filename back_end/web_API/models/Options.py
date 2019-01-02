43bfbb8f9f5113c6f98f81f86faf77a67b63da89
A-L-I-G-H <34342988+A-L-I-G-H@users.noreply.github.com>
2019-01-02 10:51:37 +0330

    participates model created

--- back_end/web_API/models/Options.py
+++ back_end/web_API/models/Options.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 
+
 class Options(models.Model):
     event_poll = models.ForeignKey('EventPolls', on_delete = models.CASCADE)
     label = models.CharField(max_length = 100)
---
+++ back_end/web_API/models/Participates.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from web_API.models.Meetings import *
+
+
+class Participates(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    meeting = models.ForeignKey(Meetings, on_delete= models.CASCADE)
+
+    class Meta:
+        db_table = 'Participates'
+        app_label = 'web_API'
